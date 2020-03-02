49f31dd8ee7119dcc053c372aec601512dcb00c9
Morteza Ziyae <m.ziaeemehr@gmail.com>
2020-03-02 23:16:09 +0330

    fix navigator

--- src/components/Navigator.svelte
+++ src/components/Navigator.svelte
@@ -1,19 +1,18 @@
-<div class="pwt-date-navigator">
-	{ currentUnixDate }
-</div>
+<div class="pwt-date-navigator">{selectedUnixDate}</div>
 
 <script>
-    import persianDate from 'persian-date'
-	export let currentUnix
-    $: currentUnixDate =  new persianDate(currentUnix).format('MMMM YYYY DD')
+  import persianDate from 'persian-date'
+  export let selectedUnix
+  $: selectedUnixDate = new persianDate(selectedUnix).format('MMMM YYYY DD')
 </script>
+
 <style global lang="scss">
-	@import './theme.scss';
-	.pwt-date-navigator {
-		width: 600px;
-		height: 30px;
-		line-height: 30px;
-		position: relative;
-		border: 3px dashed $primarycolor;
-	}
+  @import './theme.scss';
+  .pwt-date-navigator {
+    width: 600px;
+    height: 30px;
+    line-height: 30px;
+    position: relative;
+    border: 3px dashed $primarycolor;
+  }
 </style>
